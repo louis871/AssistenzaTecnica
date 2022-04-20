2e1ee7b788d9f3579dff9c108b94d132a3a3d596
louis871 <74710164+louis871@users.noreply.github.com>
2022-04-20 17:23:28 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,11 +1,22 @@
-﻿USE [assistenza_tecnica]
+USE [assistenza_tecnica]
 GO
-/****** Object:  User [assistenza01]    Script Date: 11/04/2022 20:18:03 ******/
+/****** Object:  User [assistenza01]    Script Date: 20/04/2022 17:21:47 ******/
 CREATE USER [assistenza01] FOR LOGIN [assistenza01] WITH DEFAULT_SCHEMA=[dbo]
 GO
 ALTER ROLE [db_owner] ADD MEMBER [assistenza01]
 GO
-/****** Object:  Table [dbo].[richieste]    Script Date: 11/04/2022 20:18:03 ******/
+/****** Object:  Table [dbo].[assegnati]    Script Date: 20/04/2022 17:21:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[assegnati](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nome] [varchar](127) NULL,
+	[id_utenti] [int] NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[richieste]    Script Date: 20/04/2022 17:21:47 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -13,10 +24,21 @@ GO
 CREATE TABLE [dbo].[richieste](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[testo] [varchar](max) NOT NULL,
-	[riferimento] [varchar](255) NULL
+	[id_riferimenti] [int] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[stati]    Script Date: 11/04/2022 20:18:03 ******/
+/****** Object:  Table [dbo].[riferimenti]    Script Date: 20/04/2022 17:21:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[riferimenti](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[descrizione] [varchar](255) NOT NULL,
+	[id_utenti] [int] NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[stati]    Script Date: 20/04/2022 17:21:47 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -26,7 +48,7 @@ CREATE TABLE [dbo].[stati](
 	[descrizione] [varchar](63) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[stati_richieste]    Script Date: 11/04/2022 20:18:03 ******/
+/****** Object:  Table [dbo].[stati_richieste]    Script Date: 20/04/2022 17:21:47 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -37,11 +59,11 @@ CREATE TABLE [dbo].[stati_richieste](
 	[id_stati] [int] NOT NULL,
 	[id_utenti] [int] NOT NULL,
 	[data_aggiunta] [datetime] NOT NULL,
-	[assegnazione] [varchar](127) NULL,
-	[ore_lavorate] [float] NULL
+	[ore_lavorate] [float] NULL,
+	[id_assegnati] [int] NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[utenti]    Script Date: 11/04/2022 20:18:03 ******/
+/****** Object:  Table [dbo].[utenti]    Script Date: 20/04/2022 17:21:47 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -50,6 +72,11 @@ CREATE TABLE [dbo].[utenti](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[nome] [varchar](128) NOT NULL,
 	[username] [varchar](31) NOT NULL,
-	[password] [varchar](31) NOT NULL
+	[password] [varchar](31) NOT NULL,
+	[profilo] [int] NOT NULL
 ) ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [assistenza_tecnica] SET  READ_WRITE 
 GO
