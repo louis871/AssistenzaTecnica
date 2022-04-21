2f86282e299a78dc3c01946a1efe4a39b11a6f90
louis871 <74710164+louis871@users.noreply.github.com>
2022-04-21 20:08:01 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -23,8 +23,12 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[stati](
 	[id] [int] IDENTITY(1,1) NOT NULL,
-	[descrizione] [varchar](63) NOT NULL
+	[descrizione] [varchar](63) NOT NULL,
+	[profilo_minimo] [int] NOT NULL
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[stati] ADD  CONSTRAINT [DF_stati_profilo_minimo]  DEFAULT ((0)) FOR [profilo_minimo]
 GO
 /****** Object:  Table [dbo].[stati_richieste]    Script Date: 11/04/2022 20:18:03 ******/
 SET ANSI_NULLS ON
@@ -52,4 +56,41 @@ CREATE TABLE [dbo].[utenti](
 	[username] [varchar](31) NOT NULL,
 	[password] [varchar](31) NOT NULL
 ) ON [PRIMARY]
+GO
+
+SET IDENTITY_INSERT [dbo].[stati] ON 
+GO
+INSERT [dbo].[stati] ([id], [descrizione], [profilo_minimo]) VALUES (1, N'Inserita', 0)
+GO
+INSERT [dbo].[stati] ([id], [descrizione], [profilo_minimo]) VALUES (2, N'Presa in carico', 2)
+GO
+INSERT [dbo].[stati] ([id], [descrizione], [profilo_minimo]) VALUES (3, N'Lavorazione in corso', 2)
+GO
+INSERT [dbo].[stati] ([id], [descrizione], [profilo_minimo]) VALUES (4, N'Completata', 2)
+GO
+INSERT [dbo].[stati] ([id], [descrizione], [profilo_minimo]) VALUES (5, N'Rifiutata', 3)
+GO
+INSERT [dbo].[stati] ([id], [descrizione], [profilo_minimo]) VALUES (6, N'Annullata', 1)
+GO
+INSERT [dbo].[stati] ([id], [descrizione], [profilo_minimo]) VALUES (7, N'Richiesta di ulteriori dettagli', 2)
+GO
+INSERT [dbo].[stati] ([id], [descrizione], [profilo_minimo]) VALUES (8, N'Abortita per richiesta del cliente', 1)
+GO
+INSERT [dbo].[stati] ([id], [descrizione], [profilo_minimo]) VALUES (9, N'Inseriti ulteriori dettagli', 1)
+GO
+SET IDENTITY_INSERT [dbo].[stati] OFF
+GO
+SET IDENTITY_INSERT [dbo].[utenti] ON 
+GO
+INSERT [dbo].[utenti] ([id], [nome], [username], [password], [profilo]) VALUES (1, N'Amministratore', N'admin', N'Admin1234', 4)
+GO
+INSERT [dbo].[utenti] ([id], [nome], [username], [password], [profilo]) VALUES (2, N'Operatore Due', N'oper2', N'Oper1234', 2)
+GO
+INSERT [dbo].[utenti] ([id], [nome], [username], [password], [profilo]) VALUES (3, N'Organizzatore', N'org1', N'Org123', 3)
+GO
+INSERT [dbo].[utenti] ([id], [nome], [username], [password], [profilo]) VALUES (4, N'Cliente', N'cli1', N'Cli123', 0)
+GO
+INSERT [dbo].[utenti] ([id], [nome], [username], [password], [profilo]) VALUES (5, N'Operatore Uno', N'oper1', N'Oper123', 1)
+GO
+SET IDENTITY_INSERT [dbo].[utenti] OFF
 GO
